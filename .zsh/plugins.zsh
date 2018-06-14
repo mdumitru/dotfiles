c6961a434a00c036e8bdba50c75a988f61723d8a
Mihai-Valentin DUMITRU <mihai.dumitru2201@stud.acs.upb.ro>
2018-06-14 12:03:13 +0300

    [zsh] remove git omz dependency

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -21,11 +21,11 @@ source "$manager_path"
 export ZSH="$ZPLUG_HOME/repos/robbyrussell/oh-my-zsh"
 
 
-# Useful git aliases and functions.
-zplug "plugins/git", from:oh-my-zsh
-
 # Use Ctrl-Z to switch back to background task.
 zplug "mdumitru/fancy-ctrl-z"
+
+# Useful git aliases and functions.
+zplug "mdumitru/git-aliases"
 
 # Keeps track of the last used wd and jumps into it for new shells.
 zplug "mdumitru/last-working-dir"
