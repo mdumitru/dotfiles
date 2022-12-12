90a0d46ec779ef0067c22bc00ee13aa6be5dba8e
Mihai-Valentin DUMITRU <mihai.dumitru@cs.pub.ro>
2022-12-12 10:33:20 +0200

    [x] fix capslock script for hot keyboard

--- bin/capslockcontrolesc.sh
+++ bin/capslockcontrolesc.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # Caps_Lock to Ctrl/Esc.
+
+# for some reason, it seems like when you plug in a new keyboard and have to
+# rerun the script, an older xcape process is active and the new one doesn't
+# seem to work
+pkill xcape
 setxkbmap -option 'caps:ctrl_modifier'
 xcape -e 'Caps_Lock=Escape'
