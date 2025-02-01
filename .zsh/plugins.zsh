2bf4ed4b23853fb6d675754e9887257bb7d10cf0
Mihai-Valentin DUMITRU <mihai.dumitru2201@upb.ro>
2025-02-01 10:33:33 +0200

    [zsh] switch plugin manager to zcomet

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -10,72 +10,58 @@
 # the usefulness of a plugin.
 
 
-# Attempt to source the plugin manager.
-manager_path="$HOME/.zsh/zplug/init.zsh"
-if [[ ! -r "$manager_path" ]]; then
-    return
+# Clone zcomet if necessary
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  command git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
 fi
-source "$manager_path"
 
-zplug "junegunn/fzf-bin", \
-    as:command, \
-    from:gh-r, \
-    rename-to:"fzf"
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
 # A command-line fuzzy finder
-zplug "junegunn/fzf", \
-    hook-build:"./install --bin", \
-    use:"shell/*.zsh"
+zcomet load "junegunn/fzf"
 
 # Use Ctrl-Z to switch back to background task.
-zplug "mdumitru/fancy-ctrl-z"
+zcomet load "mdumitru/fancy-ctrl-z"
 
 # Useful git aliases and functions.
-zplug "mdumitru/git-aliases"
+zcomet load "mdumitru/git-aliases"
 
 # Keeps track of the last used wd and jumps into it for new shells.
-zplug "mdumitru/last-working-dir"
+zcomet load "mdumitru/last-working-dir"
 
 # Jump to custom directories in zsh
-zplug "mfaerevaag/wd", as:command, use:"wd.sh", hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
-
-# ZSH completions for selected OS X commands.
-zplug "nilsonholger/osx-zsh-completions", if:"[[ $OSTYPE == *darwin* ]]"
+zcomet trigger wd "mfaerevaag/wd"
 
 # jump around
-zplug "rupa/z", use:"z.sh"
-
-# A next-generation plugin manager for zsh
-zplug "zplug/zplug", hook-build:"zplug --self-manage"
+zcomet trigger z "rupa/z"
 
 # zsh anything.el-like widget.
-zplug "zsh-users/zaw"
+zcomet load "zsh-users/zaw"
 
 # In older zsh versions, this seems incompatible with the syntax highlighting
 # plugin.
 if is52; then
     # Fish-like autosuggestions for zsh
-    zplug "zsh-users/zsh-autosuggestions"
+    zcomet load "zsh-users/zsh-autosuggestions"
 fi
 
 # Additional completion definitions for Zsh.
-zplug "zsh-users/zsh-completions"
+zcomet load "zsh-users/zsh-completions"
 
 # ZSH port of the FISH shell's history search
-zplug "zsh-users/zsh-history-substring-search", defer:3
+zcomet load "zsh-users/zsh-history-substring-search"
 
 # Fish shell like syntax highlighting for Zsh.
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zcomet load "zsh-users/zsh-syntax-highlighting"
 
 
 # Source 'local_plugins' file (if any).
-# This file should contain only 'zplug' directives.
 if [[ -r "$HOME/.zsh/local_plugins.zsh" ]]; then
     source "$HOME/.zsh/local_plugins.zsh"
 fi
 
 
-zplug "load"
+zcomet compinit
 
 
 # Load all plugin settings.
