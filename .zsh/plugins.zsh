9066198bba5762420b587d16a4495197bad627d5
Mihai-Valentin DUMITRU <mihai.dumitru2201@upb.ro>
2025-02-13 23:47:40 +0200

    [zsh] change z plugin

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -30,7 +30,7 @@ zcomet load "mdumitru/last-working-dir"
 zcomet trigger wd "mfaerevaag/wd"
 
 # jump around
-zcomet trigger z "rupa/z"
+zcomet load "agkozak/zsh-z"
 
 # zsh anything.el-like widget.
 zcomet load "zsh-users/zaw"
